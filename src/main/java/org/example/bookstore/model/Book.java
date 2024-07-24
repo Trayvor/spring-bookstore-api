139132b942995eacd5163015e8cdb548ea763e93
trayvor <dalv23092005@gmail.com>
2024-07-24 14:48:41 +0200

    Made some fixes with syntax

--- src/main/java/org/example/bookstore/model/Book.java
+++ src/main/java/org/example/bookstore/model/Book.java
@@ -5,19 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@RequiredArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
