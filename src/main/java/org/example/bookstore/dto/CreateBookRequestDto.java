253ad95bc6ca75fb95feebe31d73981056bcb34a
trayvor <dalv23092005@gmail.com>
2024-08-05 17:59:44 +0200

    fixed comments

--- src/main/java/org/example/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/org/example/bookstore/dto/CreateBookRequestDto.java
@@ -1,5 +1,6 @@
 package org.example.bookstore.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
@@ -8,16 +9,16 @@ import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
     @ISBN(type = ISBN.Type.ANY)
     private String isbn;
     @NotNull
     @Positive
     private BigDecimal price;
-    @NotNull
+    @NotBlank
     private String description;
     private String coverImage;
 }
